c8a14d6556ded50e7461b23bf2fb02639bd57be9
ZoranMitkovski <zoran.mitkovski@ledgerleopard.com>
2025-12-30 12:37:37 +0100

    Initial commit

---
+++ vue-project/src/main.js
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router/router.js'
+import keycloak from "./keycloak/keycloak.js";
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './assets/styles/colors.css'
+import './assets/styles/styles.css'
+import "bootstrap-icons/font/bootstrap-icons.css";
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
+
+keycloak.init({ onLoad: "login-required" }).then(() => {
+  const app = createApp(App);
+
+  // Make Keycloak instance globally available
+  app.config.globalProperties.$keycloak = keycloak;
+
+  app.mount("#app");
+});
