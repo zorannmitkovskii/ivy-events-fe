4014bcfa8daea1f4c30b5618ead6aef85d51913d
ZoranMitkovski <zoran.mikotvski1234@gmail.com>
2026-03-30 13:59:12 +0200

    Improve location handling: validate draft location in `EditInfoPanel.vue`, update `AuthLocationInput.vue` to use predictions for cleaner names and address fallback.

--- vue-project/src/components/invitations/shared/EditInfoPanel.vue
+++ vue-project/src/components/invitations/shared/EditInfoPanel.vue
@@ -17,7 +17,7 @@
 
     <div class="field">
       <label>{{ t('editSection.venue') }}</label>
-      <div v-if="!editingLocation && locationDisplay" class="location-readonly">
+      <div v-if="!editingLocation && locationDisplay && draft.location.lat != null" class="location-readonly">
         <span class="location-text">{{ locationDisplay }}</span>
         <button type="button" class="edit-location-btn" @click="editingLocation = true">
           <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"/><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"/></svg>
